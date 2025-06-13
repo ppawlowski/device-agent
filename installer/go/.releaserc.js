f319b7cd761e50997dcfd36d44a8a4477bbc5588
PPawlowski <piotr@devopswizards.com>
2025-06-13 08:44:27 +0200

    Update wroterOpts

--- installer/go/.releaserc.js
+++ installer/go/.releaserc.js
@@ -25,23 +25,23 @@ module.exports = {
       parserOpts: {
         noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
       },
-      writerOpts: {
-        // commitsSort: ['scope', 'subject'],
-        transform: (commit) => {
-          if (commit.scope !== 'installer') {
-            return false;
-          }
-          // Create a new commit object with all original properties preserved
-          // const modifiedCommit = { 
-          //   ...commit,
-          //   // Override the subject and set scope to empty string
-          //   // subject: commit.subject ? commit.subject.replace(/^installer:\s*/, '') : commit.subject,
-          //   // scope: '',
-          //   // hash: commit.hash || commit.shortHash   
-          // };
-          // return modifiedCommit;
-        }
-      }
+      // writerOpts: {
+      //   // commitsSort: ['scope', 'subject'],
+      //   transform: (commit) => {
+      //     if (commit.scope !== 'installer') {
+      //       return false;
+      //     }
+      //     // Create a new commit object with all original properties preserved
+      //     // const modifiedCommit = { 
+      //     //   ...commit,
+      //     //   // Override the subject and set scope to empty string
+      //     //   // subject: commit.subject ? commit.subject.replace(/^installer:\s*/, '') : commit.subject,
+      //     //   // scope: '',
+      //     //   // hash: commit.hash || commit.shortHash   
+      //     // };
+      //     // return modifiedCommit;
+      //   }
+      // }
     }],
     ['@semantic-release/github', {
       releaseNameTemplate: 'Installer v${nextRelease.version}',
