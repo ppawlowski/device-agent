c12b9a96b72e85ca4bb90b596035f5099365a310
PPawlowski <piotr@devopswizards.com>
2025-06-13 11:48:37 +0200

    Update release-notes config

--- installer/go/.releaserc.js
+++ installer/go/.releaserc.js
@@ -36,7 +36,7 @@ module.exports = {
             return false;
           }
           // Remove the scope from the subject line
-          commit.subject = commit.subject.replace(/installer:\s*/, '');
+          commit.subject = commit.subject.replace(/^\*\sinstaller:\s*/, '');
           return commit;
         }
         // commitPartial: "* {{subject}} ([{{hash}}]({{commitUrl}}))\n"
