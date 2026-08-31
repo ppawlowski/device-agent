2c4ec65de3ab63773561fa61555c347fc15a5fe6
Piotr Pawlowski <ppawlowski@users.noreply.github.com>
2026-08-31 17:49:28 +0200

    feat: Better message (#12)

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export function greet (name = 'world') {
-  return `hello ${name}, how are you? My name is Frankie Pulitzer.`
+  return `hello ${name}, how are you? My name is Frankie Pulitzer aka Bane.`
 }
